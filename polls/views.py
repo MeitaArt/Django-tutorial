26a31d0e1b322769a1f5de79e7ed918f2a0d6ceb
Artem <meitaartem@gmail.com>
2020-02-07 16:32:05 +0200

    part 3 already

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello world")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ','.join([q.question_text for q in latest_question_list])
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = 'You are looking at the results of questions{}'
+    return HttpResponse(response.format(question_id))
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question {}".format(question_id))
